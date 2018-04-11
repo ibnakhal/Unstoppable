ead659439f414a9efc5bb38e5cbfe17cdf32ffc0
MrPotatoSack <ibznakhal@gmail.com>
2018-04-11 11:48:13 -0700

    hacker shoots burst fire uzi shots now

--- Assets/Scripts/Enemy/HackerAI.cs
+++ Assets/Scripts/Enemy/HackerAI.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HackerAI : MonoBehaviour {
 
+    public Transform gun;
+    public GameObject bullet;
+    public Animator anim;
+    public float bulletSpeed;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,17 @@ public class HackerAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Hacker-01-Shoot"))
+        {
+            GameObject shot = Instantiate(bullet, gun.position, this.transform.rotation) as GameObject;
+            if (this.GetComponent<SpriteRenderer>().flipX == true)
+            {
+                shot.GetComponent<Rigidbody2D>().AddForce(Vector2.right * bulletSpeed);
+            }
+            else
+            {
+                shot.GetComponent<Rigidbody2D>().AddForce(Vector2.left * bulletSpeed);
+            }
+        }
 	}
 }
